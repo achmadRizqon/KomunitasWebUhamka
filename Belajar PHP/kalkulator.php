c6aeee042d51e24ecd0e7eb01c62390853d52f9c
achmadRizqon <126504179+achmadRizqon@users.noreply.github.com>
2023-09-29 23:23:36 +0700

    Add files via upload

---
+++ Belajar PHP/kalkulator.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kalkulator Biasa | Jago Ngoding</title>
+</head>
+<body>
+    <h1>Kalkulator Biasa</h1>
+    <form>
+    <?php
+        if ($_GET):
+            $a = (double) @$_GET['a'];
+            $b = (double) @$_GET['b'];
+            $operator = @$_GET['operator'];
+            
+            switch ($operator) {
+                case '+':
+                    $hasil = $a + $b;
+                    break;
+                case '-':
+                    $hasil = $a - $b;
+                    break;
+                case '*':
+                    $hasil = $a * $b;
+                    break;
+                case '/':
+                    $hasil = $a / $b;
+                    break;
+            }
+            ?>
+            <div style="margin-top: 1rem">
+            Hasil: <strong><?php echo $hasil ?></strong>
+            </div>
+            <?php
+        endif; 
+    ?>
+        <input type="number" name="a" placeholder="Bilangan a">
+
+        <select name="operator">
+            <option selected disabled>Pilih Operator</option>
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+        </select>
+
+        <input type="number" name="b" placeholder="Bilangan b">
+
+        <div style="margin-top: 1rem">
+            <button type="button" onclick="location.href = '?clear'">Clear</button>
+            <button type="submit">Hitung</button>
+        </div>
+    </form>
+</body>
+</html>
